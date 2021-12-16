5f144f67f7804d2f33cb2a58e2e9cf3b628c8de9
unknown <nur-sin_sena@hotmai.com>
2021-12-16 12:13:49 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const ejs = require("ejs");
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+// MIDDLEWARES
+app.use(express.static("public"));
 
+//ROUTES
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    name: "Blog name",
-    description: "Blog description",
-  };
-  res.send(blog);
+  //   const blog = {
+  //     id: 1,
+  //     name: "Blog name",
+  //     description: "Blog description",
+  //   };
+  //   res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 3000;
